a16c6f92e50b7a634bf7c4787ab0ef0efa461ed1
wilwil186 <edujerezwilson@gmail.com>
2022-01-12 17:44:33 -0500

    validations_query

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #Python
-from typing import Optional 
+from typing import Optional
+from fastapi.param_functions import Query 
 #Pydantic
 from pydantic import BaseModel #capaz de crear modelos
 #FastAPI
 from fastapi import FastAPI # impotar FastAPI
 from fastapi import Body # es una clase de fast que permite decir que un paramtro es de tipo body
+from fastapi import Query
 app = FastAPI() #instancia
 
 #Models
@@ -24,3 +26,12 @@ def home():
 @app.post("/person/new")
 def create_person(person: Person = Body(...)): #un request body tambien es parametro 
     return person
+
+# Validaciones: Query parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...)
+): 
+    return {name: age}
